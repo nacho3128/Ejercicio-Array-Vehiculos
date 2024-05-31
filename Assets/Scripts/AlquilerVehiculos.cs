fd7b353cdbdc81be680bfa68ee64adb31a19941e
nacho3128 <ignaciosdelavega@gmail.com>
2024-05-31 08:58:19 -0300

    Ejercicio 1 hecho

--- Assets/Scripts/AlquilerVehiculos.cs
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AlquilerVehiculos : MonoBehaviour
 {
-    public Vehiculo[] vehiculos;
+    public GameObject[] vehiculos;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +17,22 @@ public class AlquilerVehiculos : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-
-        }   
+            Desactivartodosloselementos();
+            vehiculos[Random.Range(0,vehiculos.Length) ].SetActive(true);
+        }
     }
 
     void ResetearVehiculos()
     {
-
+        //desactivar todos los elementos del array
+        //activar solo el primer elemento
+        
+    }
+    void Desactivartodosloselementos()
+    {
+    for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
     }
 }
